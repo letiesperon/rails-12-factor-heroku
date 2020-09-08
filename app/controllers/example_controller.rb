2efad7ca84a909ea4756a25677b5051e92055213
letiesperon <esperonleticia@gmail.com>
2020-09-07 22:24:40 -0300

    Read from redis

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -4,7 +4,19 @@ class ExampleController < ApplicationController
   end
 
   def cual_es_mi_nombre_var
-    my_name = ENV['NAME']
+    my_name = ENV.fetch('NAME')
+
+    render json: { message: my_name }
+  end
+
+  def cual_es_mi_nombre_redis
+    param_my_name = params[:name]
+
+    if param_my_name
+      Rails.cache.write('name', param_my_name)
+    end
+
+    my_name = Rails.cache.fetch('name')
 
     render json: { message: my_name }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'example#home'
   get '/cual_es_mi_nombre_var' => 'example#cual_es_mi_nombre_var'
+  get '/cual_es_mi_nombre_redis' => 'example#cual_es_mi_nombre_redis'
 end
