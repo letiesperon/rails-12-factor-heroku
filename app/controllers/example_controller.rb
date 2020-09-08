48f72d3eaf9f5384cece15f4651cc51c5be3e025
letiesperon <esperonleticia@gmail.com>
2020-09-07 22:06:52 -0300

    Read from env var

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -2,4 +2,10 @@ class ExampleController < ApplicationController
   def home
     render json: { message: 'welcome2!' }
   end
+
+  def cual_es_mi_nombre_var
+    my_name = ENV['NAME']
+
+    render json: { message: my_name }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/' => 'example#home'
+  get '/cual_es_mi_nombre_var' => 'example#cual_es_mi_nombre_var'
 end
