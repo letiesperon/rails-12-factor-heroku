28bc5c6a3c4f17c49b6a449d9e86089fb5bfbdff
letiesperon <esperonleticia@gmail.com>
2021-04-05 18:58:22 -0300

    Revert "Tmp"
    This reverts commit d85e641425cfa7024cdc977eedc74c2c1a75459c.

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -2,22 +2,4 @@ class ExampleController < ApplicationController
   def home
     render json: { message: 'Welcome!' }
   end
-
-  def cual_es_mi_nombre_var
-    my_name = ENV['NAME']
-
-    render json: { message: my_name }
-  end
-
-  def cual_es_mi_nombre_redis
-    Rails.cache.write('name', params[:name]) if params[:name]
-    my_name = Rails.cache.fetch('name')
-
-    render json: { message: my_name }
-  end
-
-  def cual_es_mi_nombre_global
-    $name_from_global = params[:name] if params[:name]
-    render json: { message: $name_from_global }
-  end
 end
